9683908320ca0b236f946966cf7178218535c97c
Saurabh Jamadagni <saurabhjamadagni2002@gmail.com>
2022-05-30 17:10:38 +0530

    Initial Commit

---
+++ Setsy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Setsy
+//
+//  Created by Saurabh on 30/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
