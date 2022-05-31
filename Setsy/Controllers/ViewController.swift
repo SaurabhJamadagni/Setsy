7b399695531e9050de7ab95d75ab9a8dcfa6c7e8
Saurabh Jamadagni <saurabhjamadagni2002@gmail.com>
2022-05-31 18:24:42 +0530

    Establishing connection between UIPicker and PickerDataSource

--- Setsy/Controllers/ViewController.swift
+++ Setsy/Controllers/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var eventPicker: UIPickerView!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    var eventManager = EventManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        eventPicker.dataSource = self
+        eventPicker.delegate = self
+        
+        searchTextField.borderStyle = UITextField.BorderStyle.roundedRect
+        
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return eventManager.selectionOptions.count
     }
 
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return eventManager.selectionOptions[row]
+    }
 
 }
 
--- Setsy/Models/EventManager.swift
+++ Setsy/Models/EventManager.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
-struct PickerViewData {
+struct EventManager {
+    
     let selectionOptions = ["Sunrise", "Sunset"]
 }
