93f07ad5529e7aa042ede1d88d56ff621fae1110
Saurabh Jamadagni <saurabhjamadagni2002@gmail.com>
2022-05-31 18:07:03 +0530

    Initial UI storyboarding & UIPicker choices

---
+++ Setsy/Models/PickerViewData.swift
@@ -0,0 +1,12 @@
+//
+//  PickerViewData.swift
+//  Setsy
+//
+//  Created by Saurabh on 30/05/22.
+//
+
+import Foundation
+
+struct PickerViewData {
+    let selectionOptions = ["Sunrise", "Sunset"]
+}
